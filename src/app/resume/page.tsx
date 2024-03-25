cada9ca53864c9cb87faea4b15ac03403675aeae
jklasd2001 <jklasd2001@gmail.com>
2024-03-25 23:02:23 +0900

    feat: pretendard 폰트 적용

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,13 @@
+import { Box, Container } from 'styled-system/jsx'
+
+const Resume = () => {
+  return (
+    <Box css={{ bgColor: 'grey' }}>
+      <Container>
+        <Box>aspodkasopdasdasdk</Box>
+      </Container>
+    </Box>
+  )
+}
+
+export default Resume
